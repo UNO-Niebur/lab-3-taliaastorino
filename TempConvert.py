dd987c7a1cf26f42f79e15f698309a1c6449f77b
taliaastorino <tastorino@unomaha.edu>
2026-02-08 22:37:39 -0600

    Update TempConvert.py
    test

--- TempConvert.py
+++ TempConvert.py
@@ -1,13 +1,15 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
-
+#Name: Talia Astorino
+#Date: 02/08/2026
+#Purpose: Using boolean logic, conditional statements, loops, and program flow control.
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  tempF = float(input("Enter a temeperature in fahrenheit. ")
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  tempC = round((tempF - 32) * 5/9, 1)
   #Output converted temperature.
+  
   tempF = 80
 
   tempC = tempF / 2
